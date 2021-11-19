33511b2d70e01dc6e879c093da43be89ced0be04
iihito-github <iihito.mobile@gmail.com>
2021-11-19 22:41:10 +0900

    最初のコミット

---
+++ src/com/sikimu/osero/Main.java
@@ -0,0 +1,23 @@
+package com.sikimu.osero;
+
+import java.util.Scanner;
+
+/**
+ * 実行クラス
+ * @author sikimu
+ *
+ */
+public class Main {
+
+	/**
+	 * 起動クラス
+	 * 最初に呼ばれるクラス
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int i = scanner.nextInt();
+		System.out.println(i);
+	}
+
+}
